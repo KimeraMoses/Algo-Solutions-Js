0acdd8245b284307d9f4dd6a0afa5aa9bce715fc
KimeraMoses <kimeramoses001@gmail.com>
2022-10-26 14:23:31 +0300

    new: generateHashtag function

--- script.js
+++ script.js
@@ -157,49 +157,76 @@ collection = [
 ];
 helper = new PaginationHelper(collection, 10);
 
-Test.expect(helper.pageCount() == 3, "pageCount is returning incorrect value.");
+// Test.expect(helper.pageCount() == 3, "pageCount is returning incorrect value.");
 
-Test.expect(
-  helper.pageItemCount(1) == 10,
-  "pageItemCount is returning incorrect value."
-);
-Test.expect(
-  helper.pageItemCount(2) == 4,
-  "pageItemCount is returning incorrect value"
-);
-Test.expect(
-  helper.pageItemCount(3) == -1,
-  "pageItemCount is returning incorrect value"
-);
-Test.expect(
-  helper.pageIndex(40) == -1,
-  "pageIndex returned incorrect value when provided a itemIndex argument that was out of range"
-);
+// Test.expect(
+//   helper.pageItemCount(1) == 10,
+//   "pageItemCount is returning incorrect value."
+// );
+// Test.expect(
+//   helper.pageItemCount(2) == 4,
+//   "pageItemCount is returning incorrect value"
+// );
+// Test.expect(
+//   helper.pageItemCount(3) == -1,
+//   "pageItemCount is returning incorrect value"
+// );
+// Test.expect(
+//   helper.pageIndex(40) == -1,
+//   "pageIndex returned incorrect value when provided a itemIndex argument that was out of range"
+// );
 
-Test.expect(helper.pageIndex(22) == 2, "pageIndex returned incorrect value");
-Test.expect(helper.pageIndex(3) == 0, "pageIndex returned incorrect value");
-Test.expect(
-  helper.pageIndex(0) == 0,
-  "pageIndex returned incorrect value. pageIndex(0) should return 0"
-);
-Test.expect(
-  helper.pageIndex(-1) == -1,
-  "pageIndex returned incorrect value when provided a itemIndex argument that was out of range. pageIndex(-1) should return -1"
-);
-Test.expect(
-  helper.pageIndex(-23) == -1,
-  "pageIndex returned incorrect value when provided a item_index argument that was out of range. pageIndex(-23) shoudl return -1"
-);
-Test.expect(
-  helper.pageIndex(-15) == -1,
-  "pageIndex returned incorrect value when provided a item_index argument that was out of range."
-);
+// Test.expect(helper.pageIndex(22) == 2, "pageIndex returned incorrect value");
+// Test.expect(helper.pageIndex(3) == 0, "pageIndex returned incorrect value");
+// Test.expect(
+//   helper.pageIndex(0) == 0,
+//   "pageIndex returned incorrect value. pageIndex(0) should return 0"
+// );
+// Test.expect(
+//   helper.pageIndex(-1) == -1,
+//   "pageIndex returned incorrect value when provided a itemIndex argument that was out of range. pageIndex(-1) should return -1"
+// );
+// Test.expect(
+//   helper.pageIndex(-23) == -1,
+//   "pageIndex returned incorrect value when provided a item_index argument that was out of range. pageIndex(-23) shoudl return -1"
+// );
+// Test.expect(
+//   helper.pageIndex(-15) == -1,
+//   "pageIndex returned incorrect value when provided a item_index argument that was out of range."
+// );
 
-Test.expect(helper.itemCount() == 24, "itemCount returned incorrect value");
+// Test.expect(helper.itemCount() == 24, "itemCount returned incorrect value");
 
-helper = new PaginationHelper([], 10);
-Test.assertEquals(
-  helper.pageIndex(0),
-  -1,
-  "pageIndex(0) called when there was an empty collection"
-);
+// helper = new PaginationHelper([], 10);
+// Test.assertEquals(
+//   helper.pageIndex(0),
+//   -1,
+//   "pageIndex(0) called when there was an empty collection"
+// );
+
+function generateHashtag(str) {
+  const validChar = str.split(" ").filter((string) => string !== "");
+  const hashArray = [];
+  for (const string in validChar) {
+    const newString = validChar[string];
+    hashArray.push(newString.charAt(0).toUpperCase() + newString.slice(1));
+  }
+  if (hashArray.join("").length > 139 || hashArray.join("").length === 0) {
+    return false;
+  } else {
+    return `#${hashArray.join("")}`;
+  }
+}
+
+console.log(
+  generateHashtag(
+    "Looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong Cat"
+  )
+); //false
+console.log(
+  generateHashtag(
+    "Aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+  )
+); //false
+
+console.log(generateHashtag("Moxhus is great")); //#MoxhusIsGreat
